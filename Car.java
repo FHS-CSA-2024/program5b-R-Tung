121069e40f88326284ac0ea92d398c926b800a99
R-Tung <107238461+R-Tung@users.noreply.github.com>
2024-10-08 14:56:03 -0700

    Update Car.java
    fixed

--- Car.java
+++ Car.java
@@ -45,29 +45,7 @@ import java.util.Scanner;
         return carName + " averaged " + calculateAverage() + "m/g"; 
     }
 }
-class CarTester{
-    public static Car addCar(){
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Please enter the car's name: ");
-        String name = scanner.nextLine();
-        
-        System.out.println("Please enter cars miles: ");
-        double miles = scanner.nextDouble();
-        
-        System.out.println("Please enter cars gallons: ");
-        double gallons = scanner.nextDouble();
-        
-        return new Car(name, miles, gallons);
-    }
-    public static void main (String[] args){
-        Car car1 = addCar();
-        System.out.println(car1.toString());
-        System.out.println("------NEXT CAR-------------------------------------------");
-        Car car2 = addCar();
-        System.out.println(car2.toString());
-    }
-}
+
         
 
 //Paste console output below:
